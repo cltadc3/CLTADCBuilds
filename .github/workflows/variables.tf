c8d6932a98ab0dedaf02b7da44f59e9da60559e4
cltadc3 <130259205+cltadc3@users.noreply.github.com>
2024-02-21 13:08:34 -0500

    Add files via upload

---
+++ .github/workflows/variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "common_tags" {
+  type = map(string)
+  default = {
+    Environment = "Production"
+    Department  = "Finance"
+  }
+}
+
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
